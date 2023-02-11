200d09b2855bfee0f4f1b80c74e523e41dd56b28
AfzalSabbir <afzalbd1@gmail.com>
2023-02-11 07:26:38 +0600

    Bumped `package.json` version to `0.1.0-alpha-4`
    ## [0.1.0-alpha-4] - 2023-02-11
    
    ### Added
    
    - Create `CHANGELOG.md` file to track changes
    - Added `componentNameKebabCase` in method `createFile` in `lib/create-files.js`
    - Added `.yml` file
    
    ### Changed
    
    - Bumped `package.json` version to `0.1.0-alpha-4`
    - Updated `title` in `stubs/index.html.ejs`

--- lib/create-files.ts
+++ lib/create-files.ts
@@ -35,6 +35,7 @@ const createFile = (options, replaceData) => {
   const newOptions = options;
   const replaces = {};
   newOptions.componentNamePascalCase = Case.pascal(newOptions.componentName);
+  newOptions.componentNameKebabCase = Case.kebab(newOptions.componentName);
   newOptions.ts = newOptions.language === "ts";
 
   // read any file src/vite.config.js
